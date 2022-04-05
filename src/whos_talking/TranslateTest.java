929035986f53fa35b4cc85e74ec392ac9139bc00
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-05 13:57:52 +1200

    Changes to TranslateTest.java
    Added tests for pronouns
    
    added tests for length of phrases and for irrelevant input

--- src/whos_talking/TranslateTest.java
+++ src/whos_talking/TranslateTest.java
@@ -1,13 +1,54 @@
 package whos_talking;
 
 import org.junit.Test;
+import whos_talking.errors.PhraseLengthError;
+import whos_talking.errors.PronounError;
+import whos_talking.errors.TranslateError;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TranslateTest {
 
+    /**
+     * Translate object used for testing
+     */
+    private final Translate translate = new Translate();
+
     @Test
-    public void testSomething(){
-        assertFalse(true == true);
+    public void testPronouns() {
+        // Test incorrect input
+        assertThrows(PronounError.class, () -> translate.translatePhraseHelper("we (3 excl) are going"));
+        assertThrows(PronounError.class, () -> translate.translatePhraseHelper("They (3excl) are going"));
+        assertThrows(PronounError.class, () -> translate.translatePhraseHelper("They are going"));
+        assertThrows(PronounError.class, () -> translate.translatePhraseHelper("We saw"));
+        assertThrows(PronounError.class, () -> translate.translatePhraseHelper("He (2 excl) is seeing"));
+        assertThrows(PronounError.class, () -> translate.translatePhraseHelper("He is is seeing"));
+
+        // Test with correct input
+        // TODO update these with implementation!
+        try {
+            translate.translatePhraseHelper("We (3 excl) are going");
+            translate.translatePhraseHelper("I am going");
+            translate.translatePhraseHelper("We (2 excl) saw");
+            translate.translatePhraseHelper("She saw");
+            translate.translatePhraseHelper("He is seeing");
+        } catch (TranslateError te) {
+            te.printStackTrace();
+            fail();
+        }
+
+    }
+
+    @Test
+    public void testIrrelevantInput() {
+        assertThrows(IllegalArgumentException.class, () -> translate.translatePhraseHelper(null));
+        assertThrows(IllegalArgumentException.class, () -> translate.translatePhraseHelper(""));
+    }
+
+    @Test
+    public void testPhraseLength() {
+        assertThrows(PhraseLengthError.class, () -> translate.translatePhraseHelper("hello"));
+        assertThrows(PhraseLengthError.class, () -> translate.translatePhraseHelper("Gibberish"));
+        assertThrows(PhraseLengthError.class, () -> translate.translatePhraseHelper("He is slowly going away"));
     }
 }
