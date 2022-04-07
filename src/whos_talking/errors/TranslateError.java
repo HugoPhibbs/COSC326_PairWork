5d7856cd9b50e5a449e9e2b8327ae69916e9fbb2
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-07 14:45:59 +1200

    Changes to TranslateError
    Added another constructor and removed getMessage method

--- src/whos_talking/errors/TranslateError.java
+++ src/whos_talking/errors/TranslateError.java
@@ -8,19 +8,34 @@ public class TranslateError extends Exception {
     /**
      * Constructor for a TranslateError
      *
-     * @param msg String for the error message to be displayed to a user
+     * @param msg String for a message to be displayed to a user
      */
     public TranslateError(String msg) {
         super(msg);
     }
 
     /**
-     * Gets the message of this Error
+     * Constructor for a TranslateError
      *
-     * @return String as described
+     * @param value String for the value of a parameter that caused an error
+     * @param label String for a label for 'value'
+     * @param msg String for a message to accompany this error
      */
-    public String getMessage() {
-        return String.format("Invalid: %s", super.getMessage());
+    public TranslateError(String value, String label, String msg) {
+        super(TranslateError.errorMsg(value, label, msg));
     }
 
+    /**
+     * Formats and returns a String that describes the problem with a particular value.
+     *
+     * Made into it's own method to avoid repetition
+     *
+     * @param value String for the value that is causing an error
+     * @param label String for a label for the inputted value. I.e. 'Pronoun' or 'Verb'
+     * @param msg String for a short comment to be displayed to a user, indicating the problem
+     * @return String as described
+     */
+    private static String errorMsg(String value, String label, String msg){
+        return String.format("Invalid: %s \"%s\" is %s", label, value, msg);
+    }
 }
