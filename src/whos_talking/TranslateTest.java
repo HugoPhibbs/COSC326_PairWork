fb3d1e95dc80afe9373e0689d99214494e53432d
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-21 10:23:47 +1200

    Changes to translate test
    added new tests for extra pronouns

--- src/whos_talking/TranslateTest.java
+++ src/whos_talking/TranslateTest.java
@@ -8,6 +8,9 @@ import whos_talking.errors.VerbError;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Class for testing the 'Translate' program
+ */
 public class TranslateTest {
 
     /**
@@ -25,6 +28,13 @@ public class TranslateTest {
         assertThrows(PronounError.class, () -> translate.translatePhraseHelper("He (2 excl) is seeing"));
         assertThrows(PronounError.class, () -> translate.translatePhraseHelper("He is is seeing"));
 
+        /*
+        assertEquals("Ka haere koutou", translate.translatePhrase("You (4 incl) will go"));
+        assertEquals("Ka haere koutou", translate.translatePhrase("You (3 incl) will go"));
+        assertEquals("Ka haere koutou", translate.translatePhrase("You (100 incl) will go"));
+        assertEquals("Kei te haere mātou", translate.translatePhrase("We (4 excl) are going"));
+        */
+
         try {
             translate.translatePhraseHelper("We (3 excl) are going");
             translate.translatePhraseHelper("I am going");
@@ -35,7 +45,6 @@ public class TranslateTest {
             te.printStackTrace();
             fail();
         }
-
     }
 
     @Test
@@ -43,12 +52,12 @@ public class TranslateTest {
         assertEquals("Kei te haere mātou", translate.translatePhrase("We (3 excl) are going"));
         assertEquals("Kei te haere au", translate.translatePhrase("I am going"));
         assertEquals("Kei te pānui rāua", translate.translatePhrase("They (2 excl) are reading"));
-        assertEquals("Kei te pānui kōrua", translate.translatePhrase("You (2 incl) are reading")); //TODO
+        assertEquals("Kei te pānui kōrua", translate.translatePhrase("You (2 incl) are reading"));
         assertEquals("I haere au", translate.translatePhrase("I went"));
         assertEquals("Ka haere au", translate.translatePhrase("I will go"));
 
         assertThrows(PhraseLengthError.class, () -> translate.translatePhraseHelper("gibberish"));
-        // assertThrows(VerbError.class, () -> translate.translatePhraseHelper("We (3 excl) are coming")); // TODO
+        assertThrows(VerbError.class, () -> translate.translatePhraseHelper("We (3 excl) are coming"));
     }
 
     @Test
@@ -71,7 +80,7 @@ public class TranslateTest {
     }
 
     @Test
-    public void testverb(){
+    public void testVerbs(){
         assertThrows(TranslateError.class, () -> translate.translatePhraseHelper("We (3 excl) coming"));
         assertThrows(TranslateError.class, () -> translate.translatePhraseHelper("We (3 excl) tooo make"));
         assertThrows(TranslateError.class, () -> translate.translatePhraseHelper("We (3) are go"));
